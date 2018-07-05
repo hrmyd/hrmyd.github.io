b14600af185faa63ffaa41123b701ff7060030ac
Hiranmayi Duvvuri <hiranmayid8@gmail.com>
2018-07-05 11:18:16 -0700

    Nikola auto commit.
    Nikola version: 7.8.15

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ BLOG_AUTHOR = "Hiranmayi Duvvuri"  # (translatable)
 BLOG_TITLE = "Hiranmayi Duvvuri"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "http://hiranmayiduvvuri.com"
+SITE_URL = "http://hiranmayiduvvuri.com/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://hrmyd.github.io/"
